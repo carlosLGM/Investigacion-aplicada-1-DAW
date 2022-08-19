fc26e1aa898eb9baa7f180e24ea203406c6ea204
carlosLGM <100099447+carlosLGM@users.noreply.github.com>
2022-08-18 22:20:20 -0600

    Create parte2.js

---
+++ js/parte2.js
@@ -0,0 +1,19 @@
+var propiedad,liting="";
+var propiedad,liting="";
+
+liting+="<article><h1>¿Qué es Linting?</h1><p>Es inevitable tener errores en el código que desarrollas para una aplicación, y todos sabemos"
+liting+="que estos errores son malos, algunos causan problemas en la interfaz que generan incomodidad en los usuarios, otros comprometen la seguridad"
+liting+="del sistema o simplemente rompen todo y la aplicación deja de funcionar.<br><br>";
+
+liting+="Hay un cierto grupo de errores que pueden ser identificados y reparados antes de que tu código llegue a ser ejecutado, estos pueden ser:<br><br>";
+liting+="--Errores de sintaxis<br>";
+liting+="--Código poco intuitivo o difícil de mantener<br>";
+liting+="--Uso de 'malas prácticas'<br>";
+liting+="--Uso de estilos de código inconsistencia<br><br>";
+
+liting+="Estos errores pueden ser incluso más comunes que otros más graves dado a que son menos evidentes.<br><br>";
+liting+="Capturar errores antes de que tu código se ejecute puede salvarte, no sólo del error en si mismo, si no, también ahorrarte mucho tiempo en la cacería de esos errores.<br><br>";
+liting+="En otras palabras, es una herramienta de software que revisa y 'observa' tu código en busca de errores que puedan afectar tu código. Algunos 'linteres' incluso pueden darte sugerencias de cómo arreglar el error o incluso arreglarlo ellos mismos.<br><br>";
+
+
+document.write(liting);
